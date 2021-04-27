8edbee89149cf09ffdd2d6575c1eaa5980204676
Pandade1997 <panjiahui1997@foxmail.com>
2021-04-27 10:55:26 +0800

    gen fabnk\data

---
+++ run_prepare.sh
@@ -0,0 +1,12 @@
+aishell_data=/exdata/HOME/snie/code/asvspoof/speaker_asvspoof/dataset_min/speaker/speech
+
+## make dataset
+featdir=/exdata/HOME/snie/code/asvspoof/speaker_asvspoof/dataset_min/speaker/fbank
+datadir=/exdata/HOME/snie/code/asvspoof/speaker_asvspoof/dataset_min/speaker/data
+for x in train dev test; do
+    python3 aishell_data_prep.py  $aishell_data/$x $datadir/$x $featdir/$x || exit 1;
+done
+## make test_pair for verification
+for x in dev test; do
+    python3 make_pairs.py $datadir/$x 15 15 || exit 1;
+done
